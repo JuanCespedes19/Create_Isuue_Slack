db0bc0d75ab7036f604e13f49ee7acae00a1ccf3
juancespedesf <juancespedesf.1994@gmail.com>
2025-03-06 16:28:20 -0500

    primer commit

---
+++ manifest.ts
@@ -0,0 +1,16 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import CreateIssueDefinition from "./functions/create_issue/definition.ts";
+import CreateNewIssueWorkflow from "./workflows/create_new_issue.ts";
+
+export default Manifest({
+  name: "Workflows for GitHub",
+  description: "Bringing oft-used GitHub functionality into Slack",
+  icon: "assets/icon.png",
+  functions: [CreateIssueDefinition],
+  workflows: [CreateNewIssueWorkflow],
+  outgoingDomains: [],
+  // If your organizaiton uses a separate Github enterprise domain, add that domain to this list
+  // so that functions can make API calls to it.
+  outgoingDomains: ["api.github.com"],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
+});
